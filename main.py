ed6cca4d75d317c8955fda99818861fa929961a8
RobertoCarlosMedina <roberto.carlos.medina.dev@gmail.com>
2021-02-09 11:25:14 +0100

    Adding the program.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import os
 
-nomes = ["Joelson", "Rui", "Roberto", "Raffaele"]
+nomes = ["Joelson", "Rui", "Roberto", "Raffaele", "Rafael", "Paulo", "Josias", "Kenny", "Ronaldo", "Carlos"]
 random.shuffle(nomes)
 equipa1 = []
 equipa2 = []
@@ -10,15 +10,16 @@ for elm in nomes:
     sort = False
     while True:
         rand = random.randint(0, 100)
-        if rand>50 and len(equipa1)<2:
+        if rand>50 and len(equipa1)<len(nomes)/2:
             equipa1.append(elm)
             sort = True
-        elif rand<50 and len(equipa2)<2:
+        elif rand<50 and len(equipa2)<len(nomes)/2:
             equipa2.append(elm)
             sort = True
 
         if sort:
             break
+    print("ok")
 
 os.system("cls")
 print("Equipa 1:")
